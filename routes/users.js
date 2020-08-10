69352191902fe46dace31a6c69dd5f0b2a3b99db
Laetitia Langlois <weblaetitia@hotmail.com>
2020-08-10 14:18:04 +0200

    create route add mobile users manualy

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,54 @@
 var express = require('express');
 var router = express.Router();
+var SHA256 = require('crypto-js/sha256')
+var encBase64 = require('crypto-js/enc-base64')
+var uid2 = require('uid2')
 
-var userModel = require('../models/userModel')
+var UserModel = require('../models/userModel');
+
 
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+/* POST user */
+/*$$$$$$$$$$$ route de test à supprimer en prod $$$$$$$$$$*/
+/* a utiliser avec postman pour ajouter des utilisateurs */
+
+router.post('/mobile/adduser', async function(req, res, next) {
+  
+  // chek if user alredy exist
+  var userExist = await UserModel.findOne({
+    email: req.body.email
+  })
+  if (userExist != null) {
+    res.json({
+      succes: false,
+      alert: 'User with this email already exists'
+    })
+  } else {
+    console.log('dans la route')
+    // encrypt password
+    var userSalt = uid2(32)
+    // connexion à la db
+    var newUser = await new UserModel({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      password: SHA256(req.body.password + userSalt).toString(encBase64),
+      token: uid2(32),
+      salt: userSalt,
+    })
+    var userSaved = await newUser.save()
+    console.log(userSaved)
+    res.json({
+      succes: true,
+      alert: 'New user saved'
+    })
+  }
+})
 
 
 
