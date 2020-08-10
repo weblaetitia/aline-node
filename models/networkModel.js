c6fbfcc0865a8716c40dd919a84f8dcc587adb09
chryssa volaris <chryssavolaris@MacBook-Air-de-chryssa.local>
2020-08-10 15:00:50 +0200

    resolved conflicts

--- models/networkModel.js
+++ models/networkModel.js
@@ -1,10 +1,6 @@
 const mongoose = require('mongoose');
 
-<<<<<<< HEAD
-var ProductShema = mongoose.Schema({
-=======
-var productSchema = mongoose.Schema({
->>>>>>> 7434a5e7b8b2cdfef53fba44d9a3d37403ef0280
+var ProductSchema = mongoose.Schema({
     name: String,
     brand: String,
     type: String,
@@ -15,15 +11,7 @@ var productSchema = mongoose.Schema({
     imageUrl: String
 });
 
-<<<<<<< HEAD
 var NetworkSchema = mongoose.Schema({
-    name : String,
-    phone: Number,
-    adress : String,
-    zipCode: Number,
-    city: String,
-=======
-var networkSchema = mongoose.Schema({
     firstName : String,
     lastName : String,
     businessName : String,
@@ -31,17 +19,12 @@ var networkSchema = mongoose.Schema({
     adress : String,
     zipCode: Number,
     city : String,
->>>>>>> 7434a5e7b8b2cdfef53fba44d9a3d37403ef0280
     email: String,
     webSite: String,
     refoundType: String,
     imageUrl: String,
     zoneAction: Array,
-<<<<<<< HEAD
-    products: [ProductShema]
-=======
-    products: [productSchema]
->>>>>>> 7434a5e7b8b2cdfef53fba44d9a3d37403ef0280
+    products: [ProductSchema]
    });
 
 const NetworkModel = mongoose.model('networks', NetworkSchema);
