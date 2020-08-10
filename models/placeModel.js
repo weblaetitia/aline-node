d7e10c4b52ccc2ec401617596c4dca4d12c0241d
Laetitia Langlois <weblaetitia@hotmail.com>
2020-08-10 14:45:21 +0200

    resolve conflicts

--- models/placeModel.js
+++ models/placeModel.js
@@ -1,10 +1,6 @@
 const mongoose = require('mongoose');
 
-<<<<<<< HEAD
 var ProductShema = mongoose.Schema({
-=======
-var productSchema = mongoose.Schema({
->>>>>>> 7434a5e7b8b2cdfef53fba44d9a3d37403ef0280
     name: String,
     brand: String,
     type: String,
@@ -28,11 +24,7 @@ var PlaceSchema = mongoose.Schema({
     description: String,
     network: String,
     services: Array,
-<<<<<<< HEAD
     products: [ProductShema]
-=======
-    products: [productSchema]
->>>>>>> 7434a5e7b8b2cdfef53fba44d9a3d37403ef0280
    });
 
 const PlaceModel = mongoose.model('places', PlaceSchema);
