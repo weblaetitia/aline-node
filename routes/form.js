6fbbd3b341df0d23bd797c0a3b14dcecac5c72e3
-g <-g>
2020-08-11 12:39:09 +0200

    branchement API google places et fromulaire resto

--- routes/form.js
+++ routes/form.js
@@ -20,6 +20,19 @@ router.get('/formRestaurant', function(req, res, next) {
   res.render('form/formRestaurant');
   });
 
+
+/* GET API. */
+router.post('/update-formRestaurant', async function(req, res, next){
+  console.log('placeName', req.body.placeName)
+  var data = request("GET", 'https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input='+ req.body.placeName +'&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&key=AIzaSyDQaLlOzRuURHp-Hms_PRoh9YI69tCm8nw')
+  var dataAPI = JSON.parse(data.getBody())
+  console.log('dataGoogle', dataAPI)
+
+  res.render('form/formRestaurant')
+})
+
+
+
 /* GET form for shop page. */
 router.get('/formShop', function(req, res, next) {
   res.render('form/formShop');
