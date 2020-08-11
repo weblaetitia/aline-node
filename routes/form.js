433cdd5033d65dffad2d25d4517cc9c959e7f7eb
-g <-g>
2020-08-11 13:03:24 +0200

    branchement API google places avec shop form

--- routes/form.js
+++ routes/form.js
@@ -20,8 +20,13 @@ router.get('/formRestaurant', function(req, res, next) {
   res.render('form/formRestaurant');
   });
 
+/* GET form for shop page. */
+router.get('/formShop', function(req, res, next) {
+  res.render('form/formShop');
+  });
 
-/* GET API. */
+
+/* GET API for restaurant form. */
 router.post('/update-formRestaurant', async function(req, res, next){
   console.log('placeName', req.body.placeName)
   var data = request("GET", 'https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input='+ req.body.placeName +'&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&key=AIzaSyDQaLlOzRuURHp-Hms_PRoh9YI69tCm8nw')
@@ -32,11 +37,16 @@ router.post('/update-formRestaurant', async function(req, res, next){
 })
 
 
+/* GET API for shop form. */
+router.post('/update-formShop', async function(req, res, next){
+  console.log('placeName', req.body.placeName)
+  var data = request("GET", 'https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input='+ req.body.placeName +'&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&key=AIzaSyDQaLlOzRuURHp-Hms_PRoh9YI69tCm8nw')
+  var dataAPI = JSON.parse(data.getBody())
+  console.log('dataGoogle', dataAPI)
 
-/* GET form for shop page. */
-router.get('/formShop', function(req, res, next) {
-  res.render('form/formShop');
-  });
+  res.render('form/formShop')
+})
+
 
 /* GET form for product page. */
 router.get('/formProduct', function(req, res, next) {
