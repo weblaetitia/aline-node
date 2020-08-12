382bb9c4ae00cc655231f6bf1a0bbba3c876d7e6
-g <-g>
2020-08-12 08:55:12 +0200

    token connexion et inscription network

--- routes/network.js
+++ routes/network.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var SHA256 = require('crypto-js/sha256')
+var encBase64 = require('crypto-js/enc-base64')
+var uid2 = require('uid2')
 
 var NetworkModel = require('../models/networkModel')
 
@@ -9,33 +12,81 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-router.post('/sign-in', async function(req,res,next){
 
-  var searchNetwork = await NetworkModel.findOne({
-    email: req.body.emailFromFront
-  })
-
-  if(searchNetwork!= null){
-    req.session.network = {
-      email: searchNetwork.networkemail,
-      id: searchNetwork._id
-    }
-    res.redirect('/feedChoice')
+/* GET token */
+router.get('/check-token', async function(req, res, next) {
+  console.log(req.query.token)
+  var myrequest = await networkModel.findOne({
+    token: req.query.token
+    }) 
+  console.log(myrequest)
+  if (myrequest) {
+    res.json({succes: true})
   } else {
-    res.render('signIn')
+    res.json({succes: false})
   }
-  
 })
 
+
+/* POST sign-in */
+router.post('/sign-in', async function(req,res,next){
+
+// Check if all inputs are field
+if ((req.body.emailFromFront == 'undefined') || (req.body.passwordFromFront == 'undefined')) {
+  res.json({
+    succes: false,
+    alert: 'All fields must be provided'
+  })
+} else {
+  // all fields are provided now check if user exist
+  var myrequest = await NetworkModel.find({
+    email: req.body.emailFromFront
+   }) 
+  console.log(myrequest)  
+  if (myrequest.length != 0) {
+    var hash = SHA256(req.body.passwordFromFront + myrequest[0].salt).toString(encBase64)
+    if (hash == myrequest[0].password) {
+      res.json({
+        succes: true,
+        alert: 'all good', 
+        token: myrequest[0].token
+      })
+    } else {
+      res.json({
+        succes: false,
+        alert: 'wrong password'
+      })
+    }
+
+  } else {
+    res.json({
+      succes: false,
+      alert: 'User not exists'
+    })
+  }
+}
+})
+    
+
+/* POST sign-up */
 router.post('/sign-up', async function(req,res,next){
 
-  var searchNetwork = await NetworkModel.findOne({
+// check if network alredy exist
+  var networkExist = await NetworkModel.findOne({
     email: req.body.emailFromFront 
   })
   
-  if(searchNetwork == null){
-    console.log('existe pas')
-    var newNetwork = await new NetworkModel({
+  if (networkExist != null) {
+
+    res.json({
+      succes: false,
+      alert: 'Network with this email already exists'
+    })
+  } else {
+    // encrypt password
+    var userSalt = uid2(32)
+    // add user
+     var newNetwork = await new NetworkModel({
       firstName: req.body.firstnameFromFront,
       lastName: req.body.lastnameFromFront,
       businessName : req.body.businessnameFromFront,
@@ -48,26 +99,37 @@ router.post('/sign-up', async function(req,res,next){
       refoundType: req.body.refoundtypeFromFront,
       zoneAction: req.body.zoneactionFromFront,
       password: req.body.passwordFromFront,
+      password: SHA256(req.body.password + userSalt).toString(encBase64),
+      token: uid2(32),
+      salt: userSalt,
       imageUrl : req.body.imageFromFront
     })
   
-    var newNetworkSave = await newNetwork.save();
-    console.log(newNetworkSave)
-    res.render('form/feedChoice')
-  } else {
-    console.log('existe déjà')
-    res.render('form/signUp')
-  }
-  
-})
+    var networkSaved = await newNetwork.save();
+    console.log(networkSaved)
+    // send a succes and token
+      res.json({
+        succes: true,
+        alert: 'New network saved',
+        token: networkSaved.token 
+      })
+    }
+  })
+
+//     res.render('form/feedChoice')
+//   } else {
+//     console.log('existe déjà')
+//     res.render('form/signUp')
+//   } 
+// })
 
 
+// /* Logout */
+// router.get('/logout', function(req,res,next){
 
-router.get('/logout', function(req,res,next){
+//   req.session.network = null;
 
-  req.session.network = null;
-
-  res.redirect('/')
-})
+//   res.redirect('/')
+// })
 
 module.exports = router;
