87c2f192fc42a30d0239ac32dc5bb2a2a19a65ff
Laetitia Langlois <weblaetitia@hotmail.com>
2020-08-16 17:06:36 +0200

    add script to add bar codes to network-products in db

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,9 @@ var express = require('express');
 var router = express.Router();
 var request = require('sync-request');
 
+
+var NetworkModel = require('../models/networkModel')
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('form/signIn');
@@ -12,6 +15,21 @@ router.get('/network/sign-up', function(req, res, next) {
   res.render('form/signUp');
 });
 
+
+// ajouter des barcodes dans les produits
+router.get('/add-barcode', async function(req, res, next) {
+
+  var networks = await NetworkModel.find()
+  networks.forEach((network) => {
+    network.products.forEach((prod) => {
+      var number = Math.floor((Math.random()+3)*1000000000000)
+      prod.barCode = number
+    })
+     network.save()
+  })
+  
+  res.json({sucess: true});
+});
 
 
 
