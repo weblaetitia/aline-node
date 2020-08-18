fc191924b1d1bbbd0161ec6d46d2ff19561ae22d
Laetitia Langlois <weblaetitia@hotmail.com>
2020-08-18 11:48:55 +0200

    add script to update every products keywords

--- routes/index.js
+++ routes/index.js
@@ -32,5 +32,30 @@ router.get('/add-barcode', async function(req, res, next) {
 });
 
 
+// ajouter une liste de keywords dans chaque produit
+router.get('/add-keywords', async function(req, res, next) {
+  var exclude = ['la', 'le', 'les', 'de', 'des', 'du', 'à', 'aux', 'à']
+  var networks = await NetworkModel.find()
+  networks.forEach((network) => {
+    network.products.forEach((prod) => {
+    // create keywords array
+    var keywords = [];
+    var nameSplit = prod.name.toLowerCase().split(' ')
+    var brandSplit = prod.brand.toLowerCase().split(' ')
+    var keywords = nameSplit.concat(brandSplit)
+    keywords = [...new Set(keywords)]
+
+    exclude.forEach((fil) => {
+      keywords = keywords.filter(word => word != fil)
+    })
+    console.log(keywords)
+    
+    // add keywords array to each product
+    prod.keywords = keywords
+    })
+    network.save()
+  })
+})
+
 
 module.exports = router;
