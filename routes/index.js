848b2c4bae6b312ffd65b4d779577462c6665d11
Laetitia Langlois <weblaetitia@hotmail.com>
2020-11-05 18:06:30 +0100

    send email to new subscribers

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,23 @@
 var express = require('express');
 var router = express.Router();
-var request = require('sync-request');
+const nodemailer = require("nodemailer");
+
+if(!process.env.DB_INFO){
+  require('dotenv').config()
+}
+const gmailPassword = process.env.GMAIL_PASSWORD
+
+// nodemailer config
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'aline.consigne@gmail.com',
+    pass: gmailPassword
+  }
+})
 
 
-var NetworkModel = require('../models/networkModel')
-var PlaceModel = require('../models/placeModel');
+
 const SubscriptionModel = require('../models/subscriptionModel');
 
 /* DEV MODE a efacer en prod !!!  */
@@ -43,6 +56,7 @@ router.post('/subscription', async function(req, res, next) {
   if (email) {
     res.render('index', { subscription: 'exist' })
   } else {
+
     // enregistrer l'email en bdd
     var date = new Date()
     var newSub = await SubscriptionModel( {
@@ -50,6 +64,32 @@ router.post('/subscription', async function(req, res, next) {
       email: req.body.emailFromFront
     })
     var newSubSaved = await newSub.save()
+
+    // envoyer un email
+    var mailOptions = {
+      from: '"Application Aline" <aline.consigne@gmail.com>',
+      to: req.body.emailFromFront,
+      subject: 'Nous sommes en contact',
+      text: `Merci pour l'intéret que vous portez à l'application Aline ! Soyez patient... Aline est encore en cours de développement. Vous pouvez nous suivre sur les réseaux sociaux : https://www.facebook.com/alineconsigne, https://twitter.com/aline_consigne, https://www.instagram.com/alineconsigne`,
+      html: `<h1>Merci pour l'intéret que vous portez à l'application Aline</h1>
+            <p>Soyez patient... Aline est encore en cours de développement.</p>
+            <p>Vous pouvez nous suivre sur les réseaux sociaux :</p>
+            <ul>
+              <li>Facebook : <a href="https://www.facebook.com/alineconsigne">https://www.facebook.com/alineconsigne</a></li>
+              <li>Twitter :  <a href="https://twitter.com/aline_consigne">https://twitter.com/aline_consigne</a></li>
+              <li>Instagram :  <a href="https://www.instagram.com/alineconsigne">https://www.instagram.com/alineconsigne</a></li>
+            </ul>
+            `
+    }
+    transporter.sendMail(mailOptions, function(error, info){
+      if (error) {
+        console.log(error);
+      } else {
+        console.log('Email sent: ' + info.response);
+      }
+    })
+
+
     if (newSubSaved) {
       res.render('index', { subscription: 'ok' })
     } else {
